a83c0d32e9acf48fa12aeb84c4845a44a46c9612
Cody Milliron <codymilliron.dev@gmail.com>
2024-05-03 15:21:26 -0400

    Code all the classes and get basic functionality.

--- main.py
+++ main.py
@@ -1,75 +1,76 @@
-import requests
-import selectorlib
-import smtplib, ssl
-import os
-import time
-import sqlite3
-
-"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.15'"
-
-URL = "http://programmer100.pythonanywhere.com/tours/"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-connection = sqlite3.connect("data.db")
-
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+import pandas as pd
+from pprint import pprint
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Hotel:
+    def __init__(self, id, df):
+        self.id = id
+        self.name = df[df['id'] == id].iloc[0]['name']
+        self.city = df[df['id'] == id].iloc[0]['city']
+        self.capacity = int(df[df['id'] == id].iloc[0]['capacity'])
+        self.available = df[df['id'] == id].iloc[0]['available']
+        self.df = df
+
+    def book(self):
+        try:
+            print(f'Your trip is booked in {self.name}')
+            self.available = False
+            index = df.loc[self.df['id'] == self.id].index[0]
+            self.df.at[index, "available"] = 'no'
+            # print(df.loc[self.df['id'] == self.id].index)
+            print(self.df)
+            self.df.to_csv('hotels_test.csv', index=False)
+            return True
+        except Exception as e:
+            print(f"There was an error: {e}")
+            return False
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+    def is_available(self):
+        if self.available == 'yes':
+            return True
+        else:
+            return False
 
-    username = "app8flask@gmail.com"
-    password = "qyciukmocfaiarse"
+    @staticmethod
+    def list_hotels(df):
+        print('{0: <6}'.format("ID")
+              + '{0: <30}'.format("Name")
+              + '{0: <15}'.format("City")
+              + '{0: <12}'.format("Capacity")
+              + '{0: <12}'.format("Available"))
+        for row in df.iterrows():
+            # pprint(row[1]['id'])
+            print('{0: <6}'.format(str(row[1]['id']))
+                  + '{0: <30}'.format(row[1]['name'])
+                  + '{0: <15}'.format(row[1]['city'])
+                  + '{0: <12}'.format(str(row[1]["capacity"]))
+                  + '{0: <12}'.format(row[1]['available']))
 
-    receiver = "app8flask@gmail.com"
-    context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer = customer_name
+        self.hotel = hotel_object
 
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def generate(self):
+        is_booked = self.hotel.book()
+        if is_booked:
+            print(f"Thank you {self.customer}.\nYour reservation at {self.hotel.name} is confirmed.")
+        else:
+            print("There was a problem.")
 
 
 if __name__ == "__main__":
-    while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
-        print(extracted)
+    df = pd.read_csv('hotels.csv')
 
-        if extracted != "No upcoming tours":
-            row = read(extracted)
-            if not row:
-                store(extracted)
-                send_email(message="Hey, new event was found!")
-        time.sleep(2)
+    Hotel.list_hotels(df)
+    id = int(input('Enter the id of the hotel: '))
+    hotel = Hotel(id, df)
+    print(hotel.name, hotel.city)
+    if hotel.is_available():
+        name = input('\nEnter your name: ')
+        reservation_ticket = ReservationTicket(name, hotel)
+        reservation_ticket.generate()
+    else:
+        print("\nHotel is not free.")
